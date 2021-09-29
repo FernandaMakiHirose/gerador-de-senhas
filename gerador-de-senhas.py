e91d3cb29899ba73258874e522f8251151ddf528
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-09-29 11:01:31 -0300

    Update gerador-de-senhas.py

--- gerador-de-senhas.py
+++ gerador-de-senhas.py
@@ -1,13 +1,13 @@
 import random, string
 
-password_length = int(input('Digite o tamanho da senha desejada: '))
+tamanho = int(input('Digite o tamanho da senha desejada: '))
 
 # gera senhas maiúsculas e minúsculas com os tipos definidos abaixo
-characters = string.ascii_letters + string.digits + '!@#$%&*()-+,.;?ç'
+caracteres = string.ascii_letters + string.digits + '!@#$%&*()-+,.;?ç'
 
 # gera números aleatórios fornecidos
 rnd = random.SystemRandom()
 
 # faz o print da senha
-print(''.join(rnd.choice(characters) for i in range(password_length)))
+print(''.join(rnd.choice(caracteres) for i in range(tamanho)))
 
